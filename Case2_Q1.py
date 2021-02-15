5a235741c058fd1c3b3a1f135281ac8f2f26510c
Connor <c.m.stevens@student.vu.nl>
2021-02-15 14:27:54 +0100

    Update Case2_Q1.py
    Premium payments leg loop added.

--- Case2_Q1.py
+++ Case2_Q1.py
@@ -21,8 +21,13 @@ from scipy.stats import norm
 import pandas as pd
 from scipy.integrate import quad
 ###########################################################
+"""
+Question 1
+"""
+
 #Input given variables.
 dLGD = 0.3
+dr = 0.005
 
 #Create array to hold CDS maturities and rates and other data.
 mD = {'iMaturity': [1, 3, 5, 7, 10],
@@ -30,19 +35,53 @@ mD = {'iMaturity': [1, 3, 5, 7, 10],
         'dFwd_Haz': np.zeros(5),
         'dCum_Haz': np.zeros(5),
         'dCum_D_Prob': np.zeros(5)}
-mData = pd.DataFrame(data = mD)
+mData1 = pd.DataFrame(data = mD)
 
 #Calculate cumulative hazard rates.
-mData['dCum_Haz'] = (mData['iCDS_Rate']/10000)/dLGD
+mData1['dCum_Haz'] = (mData1['iCDS_Rate']/10000)/dLGD
 
 #Loop through maturities and calculate cumulative default probability.
-for i in range(0, len(mData)):
-    mData.iloc[i, 4] = np.exp(-mData['dCum_Haz'][i] * mData['iMaturity'][i])
+for i in range(0, len(mData1)):
+    mData1.iloc[i, 4] = np.exp(-mData1['dCum_Haz'][i] * mData1['iMaturity'][i])
 
 
 #Fill in forward hazard rate for t = 1 (to simplify loop).
-mData['dFwd_Haz'][0] = -np.log(mData['dCum_D_Prob'][0]/1)/(mData['iMaturity'][0] - 0)
+mData1['dFwd_Haz'][0] = -np.log(mData1['dCum_D_Prob'][0]/1)/(mData1['iMaturity'][0] - 0)
 
 #Loop through remainder of values.
-for j in range(1, len(mData)):
-    mData['dFwd_Haz'][j] = -np.log(mData['dCum_D_Prob'][j]/mData['dCum_D_Prob'][j - 1])/(mData['iMaturity'][j] - mData['iMaturity'][j - 1])
+for j in range(1, len(mData1)):
+    mData1['dFwd_Haz'][j] = -np.log(mData1['dCum_D_Prob'][j]/mData1['dCum_D_Prob'][j - 1])/(mData1['iMaturity'][j] - mData1['iMaturity'][j - 1])
+
+"""
+Question 2
+"""
+
+#Create dataframe to hold results for question 2.
+mData2 = pd.DataFrame(data = mD)
+
+####
+#PREMIUM PAYMENTS
+####
+
+def Lam(T):
+    if ((T >= 0) and (T <= 1)):
+        CumLam = (160/10000)/dLGD
+    if ((T > 1) and (T <= 3)):
+        CumLam = (160/10000)/dLGD
+    if ((T > 3) and (T <= 5)):
+        CumLam = (160/10000)/dLGD       
+    if ((T > 5) and (T <= 7)):
+        CumLam = (160/10000)/dLGD
+    if ((T > 7) and (T <= 10)):
+        CumLam = (160/10000)/dLGD
+    return CumLam
+
+def PremPay(dr, iN):
+    dPremPay = 0
+    for iT in range(1, iN + 1):
+        dPremPay = dPremPay + np.exp(-dr * iT) * (iT - (iT - 1)) * np.exp(-quad(func =Lam,a=  0,b= iT )[0]) 
+    return dPremPay
+
+PremPay(dr, 1)
+
+quad(func =Lam,a=  0,b= 1)
